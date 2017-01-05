cb3ad36bd64eeb12578cf124cc5327355644ae26
Brian Yeh <tp26610@gmail.com>
2017-01-05 10:48:14 +0800

    add a AutoCancel options.

--- app/src/main/java/brianyeh/notificationtest/MainActivity.java
+++ app/src/main/java/brianyeh/notificationtest/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int NOTIFICATION_ID = 100;
 
-    private CheckBox cbHeadUp, cbDismiss, cbOngoing;
+    private CheckBox cbHeadUp, cbDismiss, cbOngoing, cbAutoCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         cbHeadUp = (CheckBox) findViewById(R.id.cbHeadUp);
         cbDismiss = (CheckBox) findViewById(R.id.cbDismiss);
         cbOngoing = (CheckBox) findViewById(R.id.cbOngoing);
+        cbAutoCancel = (CheckBox) findViewById(R.id.cbAutoCancel);
     }
 
     @Override
@@ -56,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     private void showNotification() {
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+        builder.setSmallIcon(android.R.drawable.ic_dialog_email)
                 .setContentTitle("this is content title")
                 .setContentText("this is content text.");
 
@@ -71,10 +72,13 @@ public class MainActivity extends AppCompatActivity {
         manager.cancel(NOTIFICATION_ID);
     }
 
+    private PendingIntent createNullPendingIntent() {
+        return PendingIntent.getActivity(this, 0, new Intent(), 0);
+    }
+
     public void addNotificationOptions(NotificationCompat.Builder builder) {
         if(cbHeadUp.isChecked()) {
-            PendingIntent pIntent = PendingIntent.getBroadcast(this, 0, new Intent(), 0);
-            builder.setFullScreenIntent(pIntent, true);
+            builder.setFullScreenIntent(createNullPendingIntent(), true);
         }
         if(cbOngoing.isChecked()) {
             builder.setOngoing(true);
@@ -83,6 +87,12 @@ public class MainActivity extends AppCompatActivity {
             Intent serviceIntent = new Intent(this, NotificationDismissService.class);
             PendingIntent pIntent = PendingIntent.getService(this, 0, serviceIntent, 0);
             builder.addAction(android.R.drawable.ic_menu_close_clear_cancel, "Dismiss", pIntent);
+        }
+        if(cbAutoCancel.isChecked()) {
+            builder.setAutoCancel(true);
+
+            // If the ContentIntent is not set, the AutoCancel won't work.
+            builder.setContentIntent(createNullPendingIntent());
         }
     }
 }
